53ecb0589a7f7c685fba6fcda76bf67d9b2a6f7c
Will Drexler <wdrexler@mojolingo.com>
2012-08-15 20:00:10 -0400

    Added code for a POC

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -1,3 +1,6 @@
+require 'net/http'
+require 'uri'
+
 class LogParser
 
   def initialize(path_to_file)
@@ -5,10 +8,14 @@ class LogParser
     @entities = {'adhearsion' => 'Adhearsion'}
     @joined_calls = []
     @log = File.open(@path)
-    @lines = @log.readlines
-    @line_count = 0
     @event_data = ""
     @post_data = ""
+  end
+
+  def start
+    read_call
+    prepare_results
+    send_results
   end
 
   def timestamped?(message)
@@ -20,26 +27,28 @@ class LogParser
   end
 
   def read_call
-    until first_event = get_event read_next_message
-      read_next_message
-    end
-    main_call = first_event[:from]
+    first_event = get_event read_next_message while first_event == nil
+    @main_call = first_event[0][:from]
+    puts @main_call
     translate first_event
-    current_event = []
-    until current_event[0][:event] == "Hangup" && current_event[0][:to] == main_call
-      translate get_event(read_next_message)
+    current_event = [{}]
+    until current_event[0][:event] == "Hangup" && current_event[0][:to] == @main_call do
+      current_event = get_event read_next_message
+      while current_event == nil do
+        current_event = get_event read_next_message 
+      end
+      translate current_event
     end
   end
 
   def read_next_message
-    until trace_message? @lines[@line_count]
-      @line_count ++
-    end
-    message = line = @lines[@line_count]
-    @line_count ++
-    until timestamped? line
+    line = ""
+    line = @log.readline until trace_message? line
+    message = line
+    line = @log.readline
+    until timestamped? line do
       message += line unless timestamped? line
-      @line_count ++
+      line = @log.readline
     end
     message
   end
@@ -48,7 +57,7 @@ class LogParser
     message_data = []
     case message
     when /RECEIVING \(presence\)/
-      call_id = extract_call_id_from_address message.split(" ")[7].delete("\"").delete("from=")
+      call_id = extract_call_id_from_address message.split(" ")[7].delete("\"").gsub("from=", '').delete("-")
       case message
       when /offer/
         from = call_id
@@ -68,8 +77,8 @@ class LogParser
         event = "Answered"
 
         message_data = [{from: from, to: to, event: event}]
-      when /joined/
-        from = [call_id, message.split("<joined ")[1].split(" ")[1].split("\"/>")[0].delete("call-id=\"")]
+      when /[^u][^n]joined/
+        from = [call_id, message.split("<joined ")[1].split(" ")[1].split("\"/>")[0].gsub("call-id=\"", '').gsub("-", '')]
         to = new_joined_call from
         event = "Joined"
 
@@ -79,7 +88,7 @@ class LogParser
 
       when /unjoined/
         from = get_joined_call call_id
-        to = [call_id, message.split("<unjoined ")[1].split(" ")[1].split("\"/>")[0].delete("call-id=\"")]
+        to = [call_id, message.split("<unjoined ")[1].split(" ")[1].split("\"/>")[0].gsub("call-id=\"", '').delete("-")]
         event = "Unjoined"
 
         to.each do |t|
@@ -92,7 +101,11 @@ class LogParser
         event = "Hangup"
 
         message_data = [{from: from, to: to, event: event}]
+      else
+        message_data = nil
       end
+    else 
+      message_data = nil
     end
     message_data
   end
@@ -102,14 +115,17 @@ class LogParser
   end
 
   def new_joined_call(calls = [])
-    @joined_calls[@joined_calls.length] = {:calls => calls, :name => "Joined Call #{@joined_calls.length + 1}", :ref => "jc#{@joined_calls.length}"}
+    @entities["jc#{@joined_calls.length}"] = "JoinedCall#{@joined_calls.length + 1}"
+    @joined_calls[@joined_calls.length] = {:calls => calls, :name => "JoinedCall#{@joined_calls.length + 1}", :ref => "jc#{@joined_calls.length}"}
+    @joined_calls.last[:ref]
   end
 
   def new_call_ref(call_id)
-    @entities["#{call_id}"] = "Call #{@entities.length + 1}"
+    @entities["#{call_id}"] = "Call#{@entities.length + 1}"
   end
 
   def get_joined_call(call_id)
+    match = false
     joined_call = nil
     @joined_calls.each do |call_ref|
       call_ref[:calls].each do |call|
@@ -119,13 +135,30 @@ class LogParser
       end
       joined_call = call_ref[:ref] if match
     end
+    joined_call
   end
 
   def translate(data)
     data.each do |event|
       new_call_ref event[:to] unless @entities.keys.include? event[:to]
       new_call_ref event[:from] unless @entities.keys.include? event[:from]
+      puts "#{event[:from]}->#{event[:to]}: #{event[:event]}\n"
       @event_data += "#{event[:from]}->#{event[:to]}: #{event[:event]}\n"
     end
+  end
+
+  private
+
+  def prepare_results
+    @post_data = "title Adhearsion Call #{@main_call}\n"
+    @entities.each do |k, v|
+      @post_data += "participant #{v} as #{k}\n"
+    end
+    @post_data += @event_data
+  end
+
+  def send_results
+    response = Net::HTTP.post_form URI.parse("http://www.websequencediagrams.com/index.php"), 'style' => 'modern-blue', 'message' => @post_data
+    puts response
   end
 end
