b2295648f243f33f3cc0639f007ab74c1f40bc64
Will Drexler <wdrexler@mojolingo.com>
2012-08-17 14:21:41 -0400

    Renamed "Message" and "Call" to "CallEvent" and "CallLog"

--- app/models/call_event.rb
+++ app/models/call_event.rb
@@ -1,9 +1,9 @@
-class Message
+class CallEvent
   include Mongoid::Document
 
   field :message,        type: String
   field :parsed_message, type: String
 
-  embedded_in :call
+  embedded_in :call_log
   validates_presence_of :message, :parsed_message
 end
--- app/models/call_log.rb
+++ app/models/call_log.rb
@@ -1,10 +1,15 @@
-class Call
+class CallLog
   include Mongoid::Document
 
   field :id,         type: String
   field :start_time, type: DateTime
   field :end_time,   type: DateTime
 
-  embeds_many :messages
+  embeds_many :call_events
   validates_presence_of :id
+
+  def translate
+    messages.each do |message|
+
+    end
 end
