428ee104526ba0494650a48a1c111cff7e65b2fa
Will Drexler <wdrexler@mojolingo.com>
2012-08-24 17:30:19 -0400

    Refactored parts of the helper

--- app/helpers/log_parse_helper.rb
+++ app/helpers/log_parse_helper.rb
@@ -12,17 +12,13 @@ module LogParseHelper
   end
 
   def read_call
-    first_event = nil
-    while first_event == nil do
-      first_event = get_event read_next_message(@stored_line)
-    end
+    first_event = get_next_event
     @main_call = first_event[0][:from]
     @call_log.id = @main_call
     translate find_dial(first_event)
     current_event = first_event
     until current_event[0][:event] == "Hangup" && current_event[0][:to] == @main_call do
-      current_event = get_event read_next_message(@stored_line)
-      current_event = get_event read_next_message(@stored_line) while current_event.nil?
+      current_event = get_next_event
       translate find_dial(current_event)
     end
     @call_log.save
@@ -54,24 +50,12 @@ module LogParseHelper
       call_id = extract_call_id_from_address message.split(" ")[7].delete("\"").gsub("from=", '').delete("-")
       case message[:message]
       when /offer/
-        from = call_id
-        to = nil
-        event = "Dial"
-
-        message_data = [{from: from, to: to, event: event}]
+        message_data = [{from: call_id, to: nil, event: "Dial"}]
       when /ringing/
-        from = call_id
-        to = call_id
-        event = "Ringing"
-
-        message_data = [{from: from, to: to, event: event}]
-        @main_call = to
+        message_data = [{from: call_id, to: call_id, event: "Ringing"}]
+        @main_call = call_id
       when /answered/
-        from = call_id
-        to = call_id
-        event = "Answered"
-
-        message_data = [{from: from, to: to, event: event}]
+        message_data = [{from: call_id, to: call_id, event: "Answered"}]
       when /[^u][^n]joined/
         from = [call_id, message.split("<joined ")[1].split(" ")[1].split("\"/>")[0].gsub("call-id=\"", '').gsub("-", '')]
         to = nil
@@ -99,11 +83,7 @@ module LogParseHelper
           message_data = nil
         end
       when /hangup/
-        from = call_id
-        to = call_id
-        event = "Hangup"
-
-        message_data = [{from: from, to: to, event: event}]
+        message_data = [{from: call_id, to: call_id, event: "Hangup"}]
       else
         message_data = nil
       end
@@ -113,27 +93,22 @@ module LogParseHelper
         channel = extract_channel_id_from_address message.split("Channel\"=>\"")[1].split("\"")[0]
         case message
         when /Ring/
-          to = channel
-          from = channel
-          event = "Ringing"
-
-          message_data = [{from: from, to: to, event: event}]
+          message_data = [{from: channel, to: channel, event: "Ringing"}]
         when /\"ChannelStateDesc\"=>\"Up\"/
           message_data = [{from: channel, to: channel, event: "Answered"}]
         else
           message_data = nil
         end
       when /ConfbridgeJoin/
-        channel = extract_channel_id_from_address message.split("Channel\"=>\"")[1].split("\"")[0]
-        from = channel
-        unless conf_bridge_exist? message.split("Conference\"=>\"")[1].split("\"")[0]
-          to = new_conf_bridge message.split("Conference\"=>\"")[1].split("\"")[0]
+        unless conf_bridge_exist? extract_conference_name(message[:message])
+          to = new_conf_bridge extract_conference_name(message[:message])
         else
-          to = message.split("Conference\"=>\"")[1].split("\"")[0]
+          to = extract_conference_name message[:message]
         end
 
-        message_data = [{from: from, to: to, event: "Joined"}]
-        @main_call = from
+        message_data = [{from: extract_channel_id_from_address extract_conference_name(message[:message]), 
+                         to: to, event: "Joined"}]
+        @main_call = extract_channel_id_from_address extract_conference_name(message[:message])
       when /ConfbridgeLeave/
         message_data = [{from: message.split("Conference\"=>\"")[1].split("\"")[0], 
                          to: extract_channel_id_from_address message.split("Channel\"=>\"")[1].split("\"")[0], 
@@ -165,20 +140,13 @@ module LogParseHelper
   def find_dial(event)
     unless event[0].nil?
       if (event[0][:event] == "Dial") && event[0][:to].nil?
-        if event[0][:from] == @main_call
+        next_event = get_next_event
+        until next_event[0][:event] == "Ringing" do
+          translate find_dial(next_event)
           next_event = get_next_event
-          until next_event[0][:event] == "Ringing" do
-            translate find_dial(next_event)
-            next_event = get_next_event
-          end
-          event[0][:to] = next_event[0][:to]
-          event += [{from: next_event[0][:to], to: next_event[0][:from], event: next_event[0][:event]}]
-        else
-          puts "Finding Dial To..."
-          dial_to = get_next_event
-          event[0][:to] = dial_to[0][:to]
-          event += [{from: dial_to[0][:to], to: dial_to[0][:from], event: dial_to[0][:event]}]
         end
+        event[0][:to] = next_event[0][:to]
+        event += [{from: next_event[0][:to], to: next_event[0][:from], event: next_event[0][:event]}]
       end
       event
     else
@@ -206,6 +174,10 @@ module LogParseHelper
 
   def extract_channel_id_from_address(address)
     address.split("/")[1].delete("-")
+  end
+
+  def extract_conference_name(message)
+    message.split("Conference\"=>\"")[1].split("\"")[0]
   end
 
   def new_call_ref(call_id)
