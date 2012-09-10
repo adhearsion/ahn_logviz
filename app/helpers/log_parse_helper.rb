acc83795446eacef8775f9b3d8579bd5ddae8d69
Will Drexler <wdrexler@mojolingo.com>
2012-09-10 12:18:39 -0400

    Unbroke the image link

--- app/helpers/log_parse_helper.rb
+++ app/helpers/log_parse_helper.rb
@@ -7,7 +7,7 @@ module LogParseHelper
   def new_chart(data, filename)
     response = Net::HTTP.post_form URI.parse("http://www.websequencediagrams.com/index.php"), 'style' => 'modern-blue', 'message' => data
     image_url = "http://www.websequencediagrams.com/" + response.body.split("{img: \"")[1].split("\"")[0]
-    file = File.open(Rails.root.join('public', 'images', "#{filename}.png"), 'wb')
+    file = File.open(Rails.root.join('public', 'assets', 'images', "#{filename}.png"), 'wb')
     file.write Net::HTTP.get(URI.parse(image_url))
     file.close
   end
