664a605ab7ca2321202573ded16b4874e0ea7166
Will Drexler <wdrexler@mojolingo.com>
2012-09-10 11:58:20 -0400

    Reorganized and renamed parts of the Adhearison Logs controller

--- app/controllers/adhearsion_logs_controller.rb
+++ app/controllers/adhearsion_logs_controller.rb
@@ -1,24 +1,18 @@
 require 'fileutils'
-include LogParseHelper
+require Rails.root.join("lib", "log_parser.rb")
 class AdhearsionLogsController < ApplicationController
 
   def create
-    render :create
   end
 
-  def process
+  def parse
     @ahn_log = AdhearsionLog.new
     uploaded_file = File.open(params[:log_file].path)
     file = File.new(Rails.root.join('public', 'uploads', "log#{AdhearsionLog.count + 1}.log"), 'w')
     FileUtils.cp(uploaded_file, file)
     @ahn_log[:log_url] = Rails.root.join 'public', 'uploads', "log#{AdhearsionLog.count + 1}.log"
+    LogParser.new(uploaded_file, @ahn_log).run
     @ahn_log.save
-    new_parser file, @ahn_log
-    until file.eof? do
-      new_call_log
-      read_call
-    end
-    file.close
-    render text: "Processing Complete!"
+    render "view"
   end
 end
