bc01615a1687aca10b6100c8be7b57265baf1145
Will Drexler <wdrexler@mojolingo.com>
2012-09-10 11:56:58 -0400

    Changed routes to accomodate for call_logs#view

--- config/routes.rb
+++ config/routes.rb
@@ -45,8 +45,9 @@ AhnLogviz::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  match 'call_logs/view/:id' => 'call_logs#view', :as => 'call_log'
 
-  match ':controller(/:action(/:id))'
+  match ':controller/:action(/:id)'
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
