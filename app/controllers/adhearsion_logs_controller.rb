d7ac1d6d828402a7ab35b66034d85dac6f041e50
Will Drexler <wdrexler@mojolingo.com>
2012-09-17 16:33:01 -0400

    Added the ability to paste logs into a text area

--- app/controllers/adhearsion_logs_controller.rb
+++ app/controllers/adhearsion_logs_controller.rb
@@ -7,11 +7,20 @@ class AdhearsionLogsController < ApplicationController
 
   def parse
     @ahn_log = AdhearsionLog.new
-    uploaded_file = File.open(params[:log_file].path)
     file = File.new(Rails.root.join('public', 'uploads', "log#{AdhearsionLog.count + 1}.log"), 'w')
-    FileUtils.cp(uploaded_file, file)
+    if params[:log_file]
+      uploaded_file = File.open(params[:log_file].path)
+      FileUtils.cp(uploaded_file, file)
+    elsif params[:pasted_text]
+      params[:pasted_text].each do |line|
+        file.write line + "\n"
+      end
+    else
+      render text: "Error! No Log File submitted!"
+      redirect_to :create
+    end
     @ahn_log[:log_url] = Rails.root.join 'public', 'uploads', "log#{AdhearsionLog.count + 1}.log"
-    LogParser.new(uploaded_file, @ahn_log).run
+    LogParser.new(File.open(@ahn_log.log_url, 'r'), @ahn_log).run
     @ahn_log.save
     render "view"
   end
