692cf49e68053de27cfe30dd2328bf02e33a4eba
Will Drexler <wdrexler@mojolingo.com>
2012-10-20 22:40:37 -0700

    Rewrote flowchart drawing in JCanvas

--- public/assets/javascripts/drawing.js
+++ public/assets/javascripts/drawing.js
@@ -10,104 +10,152 @@ function create_call_array(call_names)
   return call_array;
 }
 
-function arrow_to_self(context, x, y)
+function create_events_array(events)
 {
-  context.beginPath();
-  context.moveTo(x,y);
-  context.lineTo(x+50, y);
-  context.lineTo(x+50, y+20);
-  draw_arrow(context, x+50, y+20, x, y+20);
-  context.stroke();
-}
-
-function draw_arrow(context, fromx, fromy, tox, toy){
-  var headlen = 10;   // length of head in pixels
-  var angle = Math.atan2(toy-fromy,tox-fromx);
-  context.moveTo(fromx, fromy);
-  context.lineTo(tox, toy);
-  context.lineTo(tox-headlen*Math.cos(angle-Math.PI/6),toy-headlen*Math.sin(angle-Math.PI/6));
-  context.moveTo(tox, toy);
-  context.lineTo(tox-headlen*Math.cos(angle+Math.PI/6),toy-headlen*Math.sin(angle+Math.PI/6));
-}
-
-function findTextX(midpt, string, context)
-{
-  var x = midpt - (context.measureText(string).width / 2);
-  return x;
-}
-
-function drawEventText(from, to, y, evnt, context)
-{
-  if(to > from)
-  {
-    var midpt = from + (Math.abs(to - from) / 2);
-    context.fillText(evnt, findTextX(midpt, evnt, context), y);
-  }
-  else
-  {
-    var midpt = to + (Math.abs(from - to) / 2);
-    context.fillText(evnt, findTextX(midpt, evnt, context), y);
-  }
-}
-
-function draw_chart(events, call_names)
-{
-  var canvas = document.getElementById('drawingCanvas');
-  canvas.height = (events.length * 60 + 150).toString();
-  canvas.width = (call_names.length * 160 + 50).toString();
-  var context = canvas.getContext("2d");
-  var call_array = create_call_array(call_names);
-  var current_y = 100;
-  context.fillStyle = "#55FFFF";
-  context.strokeStyle = "#000000";
   for(i=0; i < events.length; i++)
   {
     events[i] = eval("(" + events[i] + ")")
   }
+  return events;
+}
+
+function arrowToSelf(x,y)
+{
+  $("canvas").drawLine({
+    strokeStyle: "#000",
+    strokeWidth: 1,
+    rounded: true,
+    x1: x, y1: y,
+    x2: x+50, y2: y,
+    x3: x+50, y3: y+20
+  });
+  drawArrow(x+50, y+20, x, y+20);
+}
+
+function drawArrow(from_x, from_y, to_x, to_y)
+{
+  var head = 10;
+  var angle = Math.atan2(to_y-from_y, to_x-from_x);
+  $("canvas").drawLine({
+    strokeStyle: "#000",
+    strokeWidth: 1,
+    x1: from_x, y1: from_y,
+    x2: to_x, y2: to_y,
+    x3: to_x-head*Math.cos(angle-Math.PI/6),
+    y3: to_y-head*Math.sin(angle-Math.PI/6)
+  }).drawLine({
+    x1: to_x, y1: to_y,
+    x2: to_x-head*Math.cos(angle+Math.PI/6),
+    y2: to_y-head*Math.sin(angle+Math.PI/6)
+  });
+}
+
+function createEntityGradient(x,y)
+{
+  var linear = $("canvas").createGradient({
+    x1: x, y1: y,
+    x2: x, y2: y+50,
+    c1: "#00ABEB",
+    c2: "#FFF"
+  });
+  return linear;
+}
+
+function drawEntities(call_array) {
+  var lower_y = parseInt(document.getElementById('drawingCanvas').height) - 50;
   for(i=0; i < call_array.length; i++)
   {
-    context.font = "14px Arial"
-    context.fillStyle = "#55FFFF"
-    context.fillRect(i*160, 0, 150, 50);
-    context.fillRect(i*160, parseInt(canvas.height) - 50, 150, 50);
-    context.fillStyle = "#000000";
-    context.fillText(call_array[i][0][1], i*160 + findTextX(75, call_array[i][0][1], context), 30);
-    context.fillText(call_array[i][0][1], i*160 + findTextX(75, call_array[i][0][1], context), parseInt(canvas.height) - 20);
-    context.beginPath();
-    context.moveTo(i*160 + 75, 50);
-    context.lineTo(i*160 + 75, parseInt(canvas.height) - 50);
-    context.stroke();
+    $("canvas").drawRect({
+      fillStyle: createEntityGradient(i*160,0),
+      x: i*160, y: 0,
+      width: 150, height: 50,
+      fromCenter: false,
+      cornerRadius: 10
+    }).drawRect({
+      fillStyle: createEntityGradient(i*160,lower_y),
+      x: i*160, y: lower_y,
+      width: 150, height: 50,
+      fromCenter: false,
+      cornerRadius: 10
+    });
+    $("canvas").drawText({
+      x: i*160 + 75, y: 25,
+      fillStyle: "#000",
+      font: "18px Arial",
+      fromCenter: true,
+      maxWidth: 125,
+      text: call_array[i][0][1]
+    }).drawText({
+      x: i*160 + 75, y: lower_y + 25,
+      fillStyle: "#000",
+      font: "18px Arial",
+      fromCenter: true,
+      maxWidth: 125,
+      text: call_array[i][0][1]
+    });
+    $("canvas").drawLine({
+      x1: i*160 + 75, y1: 50,
+      x2: i*160 + 75, y2: lower_y,
+      strokeStyle: "#000",
+      strokeWidth: 1
+    });
   }
-  for(i=0; i < events.length; i++)
+}
+
+function drawEvents(call_array, events_array)
+{
+  var current_y = 100;
+  for(i=0; i < events_array.length; i++)
   {
     var from = 0;
     var to = 0;
     for(j=0; j < call_array.length; j++)
     {
-      if(events[i].to.valueOf() == call_array[j][0][0].valueOf())
+      if(events_array[i].to.valueOf() == call_array[j][0][0].valueOf())
       {
         to = call_array[j][1];
       }
-      if(events[i].from.valueOf() == call_array[j][0][0].valueOf())
+      if(events_array[i].from.valueOf() == call_array[j][0][0].valueOf())
       {
         from = call_array[j][1];
       }
     }
+
     if(to == from)
     {
-      arrow_to_self(context, from, current_y);
-      context.fillStyle = "#000000";
-      context.fillText(events[i].event, to + 55, current_y + 15);
+      arrowToSelf(from, current_y);
+      $("canvas").drawText({
+        x: from + 55, y: current_y,
+        fromCenter: false,
+        font: "14px Arial",
+        fillStyle: "#000",
+        text: events_array[i].event
+        //onclick: displayEventInfo(events_array[i])
+      });
       current_y += 70;
-    }
-    else
-    {
-      context.beginPath();
-      draw_arrow(context, from, current_y, to, current_y);
-      context.stroke();
-      context.fillStyle = "#000000";
-      drawEventText(from, to, current_y - 16, events[i].event, context);
+    } else {
+      drawArrow(from, current_y, to, current_y);
+      var midpt = from + (Math.abs(to - from) / 2);
+      $("canvas").drawText({
+        x: midpt, y: current_y - 15,
+        fromCenter: true,
+        font: "14px Arial",
+        fillStyle: "#000",
+        text: events_array[i].event
+        //onclick: displayEventInfo(events_array[i])
+      });
       current_y += 50;
     }
   }
+}
+
+function drawChart(events, call_names)
+{
+  call_array = create_call_array(call_names);
+  events_array = create_events_array(events);
+  var canvas = document.getElementById('drawingCanvas');
+  canvas.height = (events.length * 60 + 150).toString();
+  canvas.width = (call_names.length * 160 + 50).toString();
+  drawEntities(call_array);
+  drawEvents(call_array, events_array);
 }
