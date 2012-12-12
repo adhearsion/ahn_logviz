2d50e3d8224643e86124a86dd1de1fee35282769
Will Drexler <wdrexler@mojolingo.com>
2012-12-12 17:23:40 -0500

    Added methods to CallLog to allow log graphing

--- app/models/call_log.rb
+++ app/models/call_log.rb
@@ -9,4 +9,20 @@ class CallLog < ActiveRecord::Base
   def end_time
     self.call_events.maximum(:time)
   end
+
+  def event_array
+    events_array = []
+    self.call_events.all.each do |event|
+      events_array += [{:log => event.log, :message => {:to => event.message.to, :from => event.message.from, :event => event.message.event}}.to_json]
+    end
+    events_array
+  end
+
+  def call_array
+    calls_array = []
+    self.calls.all.each do |call|
+      calls_array += ["#{call.ahn_call_id}", "#{call.call_name}"]
+    end
+    calls_array
+  end
 end
