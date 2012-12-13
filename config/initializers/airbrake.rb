45b8efa1be567b1c83465a1c04f00e93f106126c
Will Drexler <wdrexler@mojolingo.com>
2012-12-13 15:56:14 -0500

    Configure airbrake to work with Errbit

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,3 +1,6 @@
 Airbrake.configure do |config|
   config.api_key = ENV['AIRBRAKE_API_KEY']
+  config.host    = 'errbit.mojolingo.com'
+  config.port    = 80
+  config.secure  = config.port == 443
 end
