9c038369f0d7589c82d401c7ed5ab1861072d4e7
Will Drexler <wdrexler@mojolingo.com>
2013-01-15 18:23:24 -0500

    Starting rewrite of RayoParser (WIP)

--- lib/rayo_parser.rb
+++ lib/rayo_parser.rb
@@ -4,118 +4,73 @@ require_relative "./adhearsion_parser"
 class RayoParser < AdhearsionParser
   def initialize(log, ahn_log, line_number, pb_user)
     super(log, ahn_log, line_number, pb_user)
+    @ahn_domain = get_domain pb_user
+  end
+
+  def nokogirize!(message)
+    message = message.split(")")[1]
+    xml = Nokogiri::XML message
+    xml.remove_namespaces!
+  end
+
+  def get_domain(jid)
+    jid.split("@")[1]
   end
 
   def get_event(message)
-    time = message.match(/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/).to_s
     case message
     when /SENDING/
-      xml = Nokogiri::XML message.split(")")[1]
-      if xml.xpath("//iq").empty?
-        event_data = nil
-      else  
-        event_data = process_sent_iq message, xml.xpath("//iq")[0]
-      end
+      xml = nokogirize! message 
+      event = xml.xpath("//iq").empty? ? nil : process_sent_iq(xml)
+    when /RECEIVING \(iq\)/
+      xml = nokogirize! message
+      event = xml.xpath("//iq").empty? ? nil : process_received_iq(xml)
     when /RECEIVING \(presence\)/
-      xml = Nokogiri::XML message.split(")")[1]
-      unless xml.xpath("//presence").empty?
-        event_data = process_presence message, xml.xpath("//presence")[0] 
-      end
+      xml = nokogirize! message
+      event = xml.xpath("//presence").empty? ? nil : process_received_presence(xml)
     when /ERROR/
-      event_data = { to: @pb_user, from: @pb_user, event: ["ERROR"] }
-    else
-    end
-    make_event message, time, event_data if event_data
-  end
-
-  def process_sent_iq(message, node)
-    event_hash = {}
-    event_hash[:from] = @pb_user 
-    event_hash[:to] = node["to"]
-    case message
-    when /output/
-      event_hash[:event] = process_output_iq node
-    when /input/
-      event_hash[:event] = ["Getting Input"]  
-    when /dial/
-      event_hash[:from] = @pb_user
-      event_hash[:to] = find_dial
-      event_hash[:event] = ["Dial"]
-    when /join/
-      event_hash[:from] = process_joined_call node
-      event_hash[:event] = ["Joined"]
-    else
-      event_hash[:event] = []
-    end
-    event_hash[:event].empty? ? nil : event_hash
-  end
-
-  def process_presence(message, node)
-    event_hash = {}
-    case message
-    when /ringing/
-      event_hash[:from] = node["from"]
-      event_hash[:to] = event_hash[:from]
-      event_hash[:event] = ["Ringing"]
-    when /input/
-      event_hash[:event] = ["Received Input: \"#{node.xpath("//input").inner_text}\""]
-    when /unjoined/
-      event_hash[:from] = node["from"]
-      event_hash[:to] = find_unjoined_dest node
-      event_hash[:event] = ["Unjoined"]
-    when /hangup/
-      event_hash[:from] = node["from"]
-      event_hash[:to] = event_hash[:from]
-      event_hash[:event] = ["Hangup"]
-    else
-      event_hash[:event] = []
-    end
-    event_hash[:event].empty? ? nil : event_hash
-  end
-
-  def find_dial
-    call_domain = @pb_user.split("@")[1]
-    xml = Nokogiri::XML get_next_message.split(")")[1] 
-    if xml.xpath("//ref")[0]  
-      create_call "#{xml.xpath("//ref")[0]["id"]}@#{call_domain}"
-      "#{xml.xpath("//ref")[0]["id"]}@#{call_domain}"
-    else
-      create_call "couldnotfinddial@1.com"
-      "couldnotfinddial@1.com"
-    end
-  end
-
-  def find_unjoined_dest(node)
-    @call_log.calls.each do |call|
-      call.ahn_call_id if call.ahn_call_id =~ /#{node.xpath("//unjoined")["call-id"].gsub("-", "\-")}/
-    end
-  end
-
-  def process_output_iq(node)
-    unless (speak_node = node.xpath "//speak").empty?
-      event = []
-      event += ["Output: \"#{speak_node.inner_text}\""] unless speak_node.inner_text == ""
-      event += ["Output: Audio File"] unless node.xpath("//audio").empty?
+      event = { from: @pb_user, to: @pb_user, event: "ERROR" }
     else
       event = nil
     end
     event
   end
 
-  def process_joined_call(node)
-    if node.xpath("//join")[0]
-      call_domain = @pb_user.split("@")[1]
-      from = "#{node.xpath("//join")[0]["call-id"]}@#{call_domain}"
-      @call_log.calls.each do |call| 
-        if call.ahn_call_id == node["to"]
-          @joined_calls.each { |joined_call| joined_call[:calls_connected] += 1 if joined_call[:id] == node["to"] }
-          return from
-        end
-      end
-      @joined_calls += [{id: node["to"], calls_connected: 1}]
-      @call_log.calls.create ahn_call_id: node["to"], call_name: "Bridge #{@joined_calls.length}" 
-      from
+  def process_sent_iq(xml)
+    case xml.to_s
+    when /<join/
+      join_node = xml.xpath("//join")[0]
+      event = { from: "#{join_node['call-id']}@#{@ahn_domain}",
+                to: "#{xml.xpath("//iq")[0]['to']}",
+                event: "Join" }
+    when /<dial/
+      event = process_dial xml
+    else
+      event = nil
     end
+    event
   end
-end
 
+  def process_received_presence(xml)
+    case xml.to_s
+    when /<offer/
+      event = process_offer xml
+    else
+      event = nil
+    end
+    event
+  end
+
+  def process_dial(xml)
+    dial_node = xml.xpath("//dial")[0] 
+    master_call = @ahn_log.calls.where(sip_address: dial_node["from"]).first
+    slave_sip = dial_node["to"]
+    ref_node = nokogirize!(get_next_message).xpath("//ref")[0]
+    slave_call_id = "#{ref_node["id"]}@#{@ahn_domain}"
+    @ahn_log.calls.create(is_master: false, sip_address: slave_sip,
+                          master_call_id: master_call.id,
+                          ahn_call_id: slave_call_id)
+    { from: master_call.ahn_call_id, to: slave_call_id, event: "Dial" }
+  end
+  
+end
--- spec/lib/rayo_parser_spec.rb
+++ spec/lib/rayo_parser_spec.rb
@@ -1,3 +1,91 @@
+require 'spec_helper'
+require_relative '../../lib/rayo_parser.rb'
 describe RayoParser do
+  
+  before :each do
+    @pb_user = "fake@ahnlogviz.net"
+    @ahn_log = AdhearsionLog.create
+    @log = mock "Mock Logfile"
+    @parser = RayoParser.new(@log, @ahn_log, 1, @pb_user)
+  end
+
+  describe "#get_domain" do
+    it "should get the domain from a jabber id" do
+      @parser.get_domain("fake@ahnlogviz.net").should == "ahnlogviz.net"
+    end
+  end
+
+  describe "#get_event" do
+    it "Should run #process_sent_iq on a sent <iq>" do
+      message = "[2012-12-21 00:00:00] TRACE SENDING (iq) <iq property1=\"value1\"/>" 
+      @parser.should_receive :process_sent_iq
+      @parser.get_event message 
+    end
+
+    it "Should run #process_received_iq on a received <iq>" do
+      message = "[2012-12-21 00:00:00] TRACE RECEIVING (iq) <iq property1=\"value1\"/>"
+      @parser.should_receive :process_received_iq
+      @parser.get_event message
+    end
+
+    it "Should run #process_received_presence on a received <presence>" do
+      message = "[2012-12-21 00:00:00] TRACE RECEIVING (presence) <presence from=\"someone\">"
+      @parser.should_receive :process_received_presence
+      @parser.get_event message
+    end
+
+    it "Should return the proper ERROR event if it encounters an ERROR line" do
+      message = "[2012-12-21 00:00:00] ERROR IntruderAlert!"
+      @parser.get_event(message).should == { from: @pb_user, to: @pb_user, event: "ERROR" }
+    end
+
+    it "Should return nil if the message does not apply to the filters" do
+      message = "[2012-12-21 00:00:00] TRACE SENDING (something) <something/>"
+      @parser.get_event(message).should == nil
+    end
+  end
+
+  describe "#process_sent_iq" do
+    it "should properly process a sent <join>" do
+      message = Nokogiri::XML "<iq to=\"random_string@ahnlogviz.net\"><join call-id=\"random_string2\"></iq>"
+      @parser.process_sent_iq(message).should == { from: "random_string2@ahnlogviz.net", to: "random_string@ahnlogviz.net", event: "Join" }
+    end
+
+    it "should send <dial>s to #process_dial" do
+      message = Nokogiri::XML "<dial/>"
+      @parser.should_receive :process_dial
+      @parser.process_sent_iq message
+    end
+  end
+
+  describe "#process_received_presence" do
+    it "should send <offer>s to #process_offer" do
+      message = Nokogiri::XML "<offer/>"
+      @parser.should_receive :process_offer
+      @parser.process_received_presence message
+    end
+  end
+
+  describe "#process_dial" do
+    before :each do
+      @call = @ahn_log.calls.create(is_master: true, sip_address: "sip:sip@ahnlogviz.net", 
+                                    ahn_call_id: "random_string@ahnlogviz.net")
+    end
+
+    it "should correctly parse a <dial>" do
+      @calls = mock "Mock Calls"
+      message = Nokogiri::XML "<iq><dial from=\"sip:sip@ahnlogviz.net\" to=\"sip:sip1@ahnlogviz.net\">"
+      lines = ["[2012-12-21 00:00:00] TRACE RECEIVING (iq) <iq><ref id=\"random_string2\"/></iq>",
+               "[2012-12-21 00:00:00] DEBUG"]
+      @log.should_receive(:readline).and_return lines[0]
+      2.times do |i|
+        @log.should_receive(:readline).and_return lines[i]
+      end
+      @parser.process_dial(message).should == { from: "random_string@ahnlogviz.net",
+                                                to: "random_string2@ahnlogviz.net",
+                                                event: "Dial" }
+      @ahn_log.calls.where(ahn_call_id: "random_string2@ahnlogviz.net", is_master: false).should_not == []
+    end
+  end
 
 end
